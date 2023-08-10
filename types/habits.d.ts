05937eeff19563182456760b8041e55e89104013
Fernando Lopez <fernandojalope343@gmail.com>
2023-08-10 10:32:19 -0400

    Habits type

---
+++ types/habits.d.ts
@@ -0,0 +1,16 @@
+type ID = `${string}-${string}-${string}-${string}-${string}`;
+type Color = `#${string}`;
+type Day = "Monday" | "Tuesday" | "Wednesday" | "Thursday" | "Friday";
+
+export interface Habit {
+	readonly id: ID;
+	name: string;
+	description: string;
+	icon: string;
+	color: string;
+	requires_goal: boolean;
+	goal?: number | null;
+	frequencies: Array<Day>;
+	reminders: boolean;
+	start_time?: string | null;
+}
