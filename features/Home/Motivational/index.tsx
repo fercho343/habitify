4c9b4e657d2eff41f23ba1528394f47f8e76e356
Fernando Lopez <fernandojalope343@gmail.com>
2023-08-10 20:30:37 -0400

    add image bg to motivation

--- features/Home/Motivational/index.tsx
+++ features/Home/Motivational/index.tsx
@@ -1,14 +1,52 @@
 import { Text } from "@/components/Text";
+import moment from "moment";
 import React from "react";
-import { Box } from "./styled";
+import { useTheme } from "styled-components/native";
+import { Background, Box } from "./styled";
 
 export const Motivational = () => {
+	const theme = useTheme();
+	const isWhatMoment = getHourToday(moment());
+
 	return (
 		<Box style={{ elevation: 4 }}>
-			<Text variant="subtitle_medium">
-				No te preocupes por los fracasos, preocúpate por las oportunidades que
-				pierdes por temor a fracasar.
-			</Text>
+			<Background
+				source={
+					isWhatMoment === "morning"
+						? require("@/assets/images/morning.jpg")
+						: isWhatMoment === "afternoon"
+						? require("@/assets/images/after.jpg")
+						: require("@/assets/images/night.jpg")
+				}
+			>
+				<Text
+					variant="subtitle_medium"
+					style={{
+						color: theme.colors.text,
+						textShadowColor: "rgba(0, 0, 0, 0.75)",
+						textShadowOffset: { width: -1, height: 1 },
+						textShadowRadius: 10,
+					}}
+				>
+					No te preocupes por los fracasos, preocúpate por las oportunidades que
+					pierdes por temor a fracasar.
+				</Text>
+			</Background>
 		</Box>
 	);
+};
+
+const getHourToday = (date: any) => {
+	const hour = date.hour();
+
+	switch (hour) {
+		case hour < 12:
+			return "morning";
+
+		case hour >= 12 && hour < 18:
+			return "afternoon";
+
+		default:
+			return "night";
+	}
 };
--- features/Home/Motivational/styled.ts
+++ features/Home/Motivational/styled.ts
@@ -2,8 +2,15 @@ import { styled } from "styled-components/native";
 
 export const Box = styled.View`
   width: 100%;
+  height: 120px;
   margin-top: 40px;
+  overflow: hidden;
   border-radius: 10px;
   background-color: ${({ theme }) => theme.colors.card};
+`;
+
+export const Background = styled.ImageBackground`
+  height: 120px;
   padding: 10px;
+  justify-content: center;
 `;
