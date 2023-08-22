3803fa55832cd2e6603e83821b919946e12fd7fc
Fernando Lopez <fernandojalope343@gmail.com>
2023-08-22 19:26:39 -0400

    new start

--- app/add-habit.tsx
+++
@@ -1,6 +0,0 @@
-import { FormAddHabit } from "@/features/Home/FormAddHabit";
-import React from "react";
-
-export default function AddHabitScreen() {
-	return <FormAddHabit />;
-}
--- types/profile.d.ts
+++
@@ -1,4 +0,0 @@
-export interface ProfileType {
-	name: string;
-	picture: string;
-}
