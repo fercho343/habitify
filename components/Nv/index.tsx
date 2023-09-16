8bdd185b8420490366a21974bc502217dfe9259c
fernando343 <fernandojalope343@gmail.com>
2023-09-16 15:29:19 -0400

    add lebel

--- components/Nv/index.tsx
+++ components/Nv/index.tsx
@@ -1,5 +1,7 @@
 //@ts-nocheck
-import React from "react";
+import { useHabit } from "@/services/context/HabitContext";
+import moment from "moment";
+import React, { useEffect, useState } from "react";
 import { NlContent, NlText } from "./styled";
 
 export const Nv: React.FC<PropsNv> = ({
@@ -9,6 +11,37 @@ export const Nv: React.FC<PropsNv> = ({
 	right = 10,
 	fontSize = 10,
 }) => {
+	const { completedDays } = useHabit();
+	const [currentLevel, setCurrentLevel] = useState(1);
+
+	useEffect(() => {
+		const today = moment();
+		let consecutiveDays = 0;
+
+		// Recorre los hábitos completados en orden inverso para contar los días consecutivos
+		for (let i = completedDays.length - 1; i >= 0; i--) {
+			const completedDate = moment(completedDays[i].date);
+
+			// Compara las fechas sin tener en cuenta la hora
+			if (completedDate.isSameOrAfter(today, "day")) {
+				consecutiveDays++;
+			} else {
+				// Si no es consecutivo, detén el ciclo
+				break;
+			}
+		}
+
+		let newLevel = 1;
+		let daysRequiredForNextLevel = 1;
+
+		while (consecutiveDays >= daysRequiredForNextLevel) {
+			newLevel++;
+			daysRequiredForNextLevel += newLevel * 2 + 1; // Requiere 1 día más que el nivel anterior
+		}
+
+		setCurrentLevel(newLevel);
+	}, [currentLevel, completedDays]);
+
 	return (
 		<NlContent
 			$width={width}
@@ -17,7 +50,7 @@ export const Nv: React.FC<PropsNv> = ({
 			$right={right}
 			// style={{ width: 60, height: 60, bottom: -15, right: -10 }}
 		>
-			<NlText $fontSize={fontSize}>Nv 1</NlText>
+			<NlText $fontSize={fontSize}>Nv {currentLevel}</NlText>
 		</NlContent>
 	);
 };
