7c922f98ba6c6ee0811c06b77ce38d51b2e9dd6d
Fernando Lopez <fernandojalope343@icloud.com>
2023-09-18 19:48:23 -0400

    fix journal id

--- app/journal-id.tsx
+++ app/journal-id.tsx
@@ -1,23 +1,26 @@
+import { Text } from "@/components/Text";
 import { Body } from "@/features/journal/styled";
 import { useLocalSearchParams } from "expo-router";
+import { ScrollView } from "react-native";
 import { RichEditor } from "react-native-pell-rich-editor";
 import { useTheme } from "styled-components/native";
 
 export default function JournalIdScreen() {
 	const { item } = useLocalSearchParams();
-	const theme = useTheme();
 	//@ts-ignore
 	const newItem = JSON.parse(item);
 
+	const getPreviewText = (): string => {
+		const content: any = newItem.text;
+		const newText = content.replace(/<[^>]*>/g, "");
+		return newText;
+	};
+
 	return (
 		<Body>
-			<RichEditor
-				initialContentHTML={newItem.text}
-				editorStyle={{
-					backgroundColor: theme.colors.background,
-					color: theme.colors.text,
-				}}
-			/>
+			<ScrollView>
+				<Text>{getPreviewText()}</Text>
+			</ScrollView>
 		</Body>
 	);
 }
